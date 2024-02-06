8dd1b4b4771e3415a6c237b691ae3e083e4e102f
assanalibatyrkhan <asankek12@gmail.com>
2024-02-06 17:35:23 +0600

    Fixed all the bugs involving -(), and (-((, arrow bugs

--- dnf.py
+++ dnf.py
@@ -15,76 +15,66 @@ def get_table(n):
         table.append(temp)
     return table
 
+
 def negation(s):
     arr = s.split()
     for i in range(len(arr)):
-        #(-a) or -a
-        if ("-" in arr[i] and n == 1) or ("-" in arr[i] and len(arr[i]) == 2):
-            found = arr[i].find("-")
-            arr[i] = arr[i][:found] + arr[i][found+1:]
-        #(a) or a
-        elif ("-" not in arr[i] and n == 1) or ("-" not in arr[i] and len(arr[i]) == 1 and arr[i].isalpha()):
-            counted = arr[i].count('(')
-            arr[i] = arr[i][:counted] + '-' + arr[i][counted:]
-
-        elif arr[i] == "&":
+        if arr[i] == "&":
             arr[i] = "|"
         elif arr[i] == "|":
             arr[i] = "&"
+        # (-a
+        elif "-" in arr[i]:
+            found = arr[i].find("-")
+            # (-a or -a
+            if found == len(arr[i]) - 2:
+                arr[i] = arr[i][:found] + arr[i][found + 1:]
+            # ((-(a
+            else:
+                # if s is for instance (-(a | b) & (a & b))
+                opening, closing, j = arr[i][found:].count("("), 0, i + 1
+                while opening > closing:
+                    opening += arr[j].count("(")
+                    closing += arr[j].count(")")
+                    j += 1
+                i = j
+                continue
+        # (a
+        elif "-" not in arr[i]:
+            counted = arr[i].count('(')
+            arr[i] = arr[i][:counted] + '-' + arr[i][counted:]
 
-        # ((((a
-        elif "(" in arr[i]:
-            if "-" not in arr[i]:
-                if arr[i].count("(") != len(arr[i]):
-                    arr[i] = arr[i][:-1] + '-' + arr[i][-1]
-            #(((-a
-            elif arr[i][-2] == "-":
-                arr[i] = arr[i][:-2] + arr[i][-1]
-            # ((-((a | b) | b))) or -(((a
-            elif "-" in arr[i]:
-                found = arr[i].find("-")
-                # print(arr[i])
-                opening_parenthesis, closing_parenthesis = 0, 0
-                current_open, current_close = found+1, 0
-                for j in range(found+1, len(s)):
-                    if s[j] == "(":
-                        opening_parenthesis += 1
-                    elif s[j] == ")":
-                        closing_parenthesis += 1
-                        current_close = j
-                    if opening_parenthesis == closing_parenthesis != 0:
-                        break
-                x = s[:found] + negation(s[current_open: current_close+1])[:-1] + s[current_close+1:]
-                # print(x)
-                if n != 1:
-                    return negation(x)
-                else:
-                    arr[i] = x.split()[0]
-        #a))))
-        elif ")" in arr[i]:
-            if "-" not in arr[i]:
-                if arr[i].count(")") != len(arr[i]):
-                    arr[i] = '-' + arr[i]
-            #-a))))
-            elif "-" in arr[i]:
-                arr[i] = arr[i][1:]
     return ' '.join(arr) + ' '
 
 
 def dnf(n, s):
     ans, table = '', get_table(n)
     for i in range(len(s)):
-        # -(((a | b))) ((-(a | b)))
-        #arrow
+        # -()
+        if s.find("-(", i, len(s) - 1) != -1:
+            found = s.find("-(", i, len(s) - 1)
+            opening_parenthesis, closing_parenthesis = 0, 0
+            current_open, current_close = found, 0
+            for j in range(found, len(s)):
+                if s[j] == "(":
+                    opening_parenthesis += 1
+                elif s[j] == ")":
+                    current_close = j
+                    closing_parenthesis += 1
+                if opening_parenthesis == closing_parenthesis != 0:
+                    break
+            s = s[:found] + s[found:].replace("-(", "(", 1)
+            current_close -= 1
+            s = s[:found + 1] + negation(s[current_open + 1:current_close]) + s[current_close:]
+        # arrow
         if s.find("r", i, len(s) - 1) != -1:
             found = s.find("r", i)
             s = s.replace("r", "|", 1)
-            # print(s)
-            temp = s[:found-1]
+            temp = s[:found - 1]
             start, end, opening_parenthesis, closing_parenthesis = 0, 0, 0, 0
             current_open, current_close = 0, 0
             # print(temp)
-            for j in range(len(temp)-1, -1, -1):
+            for j in range(len(temp) - 1, -1, -1):
                 # print(temp[j])
                 if temp[j] == ")":
                     closing_parenthesis += 1
@@ -95,33 +85,15 @@ def dnf(n, s):
                 if opening_parenthesis == closing_parenthesis != 0:
                     break
             if current_close != 0:
-                s = s[:current_open] + negation(s[current_open: current_close+1]) + s[current_close+1:]
+                s = s[:current_open] + negation(s[current_open: current_close + 1]) + s[current_close + 1:]
             else:
                 s = s[:current_open] + negation(s[current_open:found]) + s[found:]
-            # -()
-            if s.find("-(", i, len(s) - 1) != -1:
-                found = s.find("-(", i, len(s) - 1)
-                opening_parenthesis, closing_parenthesis = 0, 0
-                current_open, current_close = found, 0
-                for j in range(found, len(s)):
-                    if s[j] == "(":
-                        opening_parenthesis += 1
-                    elif s[j] == ")":
-                        current_close = j
-                        closing_parenthesis += 1
-                    if opening_parenthesis == closing_parenthesis != 0:
-                        break
-                s = s[:found] + s[found:].replace("-(", "(", 1)
-                # print(s, current_close)
-                current_close -= 1
-                print(s[current_close], s[current_open + 1:current_close])
-                s = s[:found + 1] + negation(s[current_open + 1:current_close]) + s[current_close:]
-    print(s)
+    print(f"Our converted function: {s}")
     # last step with | and & and paranthesis
     for i in table:
         temp = s
+        # replacing letters with 0's and 1's from the truth table
         for j in range(len(i)):
-            # print(t)
             if j == 0:
                 temp = temp.replace("-a", str(int(not i[j])))
                 temp = temp.replace("a", str(int(i[j])))
@@ -134,10 +106,7 @@ def dnf(n, s):
             elif j == 3:
                 temp = temp.replace("-d", str(int(not i[j])))
                 temp = temp.replace("d", str(int(i[j])))
-        # print(i, end=' ')
-        # print(temp)
-        # print(eval(temp))
-
+        # checking if the result is 1, then adding it to the final answer
         if eval(temp):
             if n == 1:
                 ans += f' | {"a" if i[0] else "-a"}'
@@ -151,10 +120,9 @@ def dnf(n, s):
     return ans[3:]
 
 
-
-
-
 if __name__ == '__main__':
-    n = int(input("num of variables: "))
-    s = input("expression: ")
+    n = int(input("Enter the number of variables: "))
+    s = input("Your boolean expression: ")
     print(dnf(n, s))
+    # 2
+    # (-(a | b) & (a & b))
